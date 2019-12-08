2486e533d190293c8089a9ce15910ea9b1038c9a
huanghui <huang.hui@ciicsh.com>
2019-12-08 14:29:00 +0800

    11

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,22 +1,69 @@
 import styles from './index.scss';
-import { Form, Icon, Input, Button } from 'antd';
-export default () => {
-  return (
-    <div className={styles.contentBg}>
-      <Form layout="inline" >
-        <Form.Item >
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="Username"
-            />
-        </Form.Item>
-       
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-};
+import { Form, Icon, Input, Button, Row, Col  } from 'antd';
+import OImg from '@/assets/image/tree.png';
+
+import { Component } from 'react';
+
+class Login extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+  handleReset = () => {
+    this.props.form.resetFields();
+  };
+  render() {
+    const {getFieldDecorator} = this.props.form;
+    return(
+      <div className={styles.contentBg}>
+        <img src={OImg} alt=""/>
+        <div>
+          <h6>用户登录</h6>
+          <Form  labelCol={{ span: 7 }} wrapperCol={{ span: 12 }}  onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item label="Username" >
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Username"
+                  />,
+                )}
+            </Form.Item>
+            
+            <Form.Item label="Password" >
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                />,
+              )}
+            </Form.Item>
+          
+            <Row>
+              <Col span={7}></Col>
+              <Col span={12}>
+                <Button type="primary" htmlType="submit">
+                  登录
+                </Button>
+                
+                <Button className='ml20' onClick={this.handleReset}>
+                  重置
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </div>
+    )
+  }
+}
+const LoginForm = Form.create({ name: 'loginForm' })(Login);
+export default LoginForm;
